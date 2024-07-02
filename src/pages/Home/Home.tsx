67cab7c23bb8564d9d1a4ef227f44844bdb3274c
swaff-y <kyle@swaff.id.au>
2024-07-02 17:56:37 +1000

    F1-1: Update home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,17 @@
+import { useDrivers } from "../../hooks/useDrivers";
 import { useMeeting } from "../../hooks/useMeeting";
 import { useMeetings } from "../../hooks/useMeetings";
 
 export const Home = () => {
   const meetings = useMeetings({ year: '2024' });
-  const key = meetings.data.getIndex(1)?.meeting_key || 0;
-  const meeting = useMeeting({ meeting_key: key });
+  const meet = meetings.data.getIndex(1);
+  const meeting = useMeeting({ meeting_key: meet?.meeting_key });
+  const drivers = useDrivers({ 
+    meeting_key: 1239, 
+    session_key: 9540
+  });
+  
+  console.log('zebra', drivers)
   return (
     <div>
       <h1>Home</h1>
