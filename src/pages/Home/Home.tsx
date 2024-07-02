d5c17cf00665c45700fd27d3d4a5b5631f9430ac
swaff-y <kyle@swaff.id.au>
2024-07-02 17:11:31 +1000

    F1-1: Add Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
-
+import { useMeeting } from "../../hooks/useMeeting";
+import { useMeetings } from "../../hooks/useMeetings";
 
 export const Home = () => {
+  const meetings = useMeetings({ year: '2024' });
+  const key = meetings.data.getIndex(1)?.meeting_key || 0;
+  const meeting = useMeeting({ meeting_key: key });
   return (
     <div>
       <h1>Home</h1>
+      <p>{meeting.isSuccess && <>{meeting.data.get('meeting_name')}</>}</p>
     </div>
   );
 };
