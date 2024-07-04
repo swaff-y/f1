41bd4071b9be2482f93c8813bc437e1e57fca7bb
swaff-y <kyle@swaff.id.au>
2024-07-04 13:45:08 +1000

    F1-2: UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
 import { Home } from './pages/Home/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { OpenF1API } from './api/openF1API';
+import { OpenF1Provider } from './context/openF1';
+import { Container } from 'react-bootstrap';
+
+const queryClient = new QueryClient();
+const openF1 = new OpenF1API();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route 
-          path="/"
-          element={<Home />}
-        >
-        </Route>
-      </Routes>
-    </Router>
+    <Container fluid className="app-background">
+      <OpenF1Provider value={openF1}>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route 
+                path="/"
+                element={<Home />}
+              >
+              </Route>
+              <Route 
+                path="/home/meetings"
+                element={<Home />}
+              >
+              </Route>
+              <Route 
+                path="/home/sessions"
+                element={<Home />}
+              >
+              </Route>
+              <Route 
+                path="/home/drivers"
+                element={<Home />}
+              >
+              </Route>
+            </Routes>
+          </Router>
+        </QueryClientProvider>
+      </OpenF1Provider>
+    </Container>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,32 @@
+import './home.css'
+import { Container, Nav } from "react-bootstrap";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { useLocation } from 'react-router-dom';
 
 
 export const Home = () => {
+  let location = useLocation();
+  
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container fluid className="home-container">
+      <NavBar selected='home'/>
+        <Nav data-bs-theme="dark" variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href='/home/meetings' active={
+            location.pathname === '/home/meetings' || location.pathname === '/'
+          }>Races</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/home/sessions' active={
+            location.pathname === '/home/sessions'
+          }>Sessions</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/home/drivers' active={
+            location.pathname === '/home/drivers'
+          }>Drivers</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
   );
 };
